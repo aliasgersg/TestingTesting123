9f574b3bb3106fdee0213d96538522d2f4fe9e5f
aliasgersg <alighotawala02@gmail.com>
2023-12-05 12:29:03 -0500

    In this commit, I fixed the unit testing for remove.

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -37,7 +37,7 @@ class UniqueCollectionTest {
 
         val originalSize = collection.size()
         collection.clear()
-        val newSize = collection.size();
+        val newSize = collection.size()
 
         assert(originalSize == 2 && newSize == 0) { "Items not cleared" }
     }
@@ -45,18 +45,18 @@ class UniqueCollectionTest {
     @Test
     fun removeAllItems() {
         collection.addItem(Item("item1"))
-        collection.addItem(Item("Item2"))
+        collection.addItem(Item("item2")) // Use consistent naming
         collection.addItem(Item("item3"))
 
         val originalSize = collection.size()
-        collection.remove(Item("item2")) // Remove an item
+        collection.remove(Item("item2")) // Remove 'item2'
 
         val newSize = collection.size()
-        val isItem2Present = collection.get(0).name.equals("item2", ignoreCase = true) ||
-                collection.get(1).name.equals("item2", ignoreCase = true)
+        val isItem2Removed = (newSize == originalSize - 1) &&
+                !collection.get(0).name.equals("item2", ignoreCase = true) &&
+                !collection.get(1).name.equals("item2", ignoreCase = true)
 
-        assertTrue("Size did not decrease after removal", originalSize - newSize == 2)
-        assertFalse("Item 'item2' was not removed", isItem2Present)
+        assertTrue("Item 'item2' was not removed correctly", isItem2Removed)
     }
 }
 // identify and create tests for certain artifacts in the assignemnt 10 video, youre not supposed to test activites, things like book list or book, TEST FOR PART THAT CALCULATES HOW FAR IT REWINDS
